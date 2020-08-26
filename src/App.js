f40798f846a20723e92d3cbd214e29dd38f7dcec
Jorge Velarde <jvelarde@eclass.cl>
2020-08-25 23:46:23 -0400

    feat(ciclos): se agregan ciclos para leer archivos

--- src/App.js
+++ src/App.js
@@ -1,42 +1,67 @@
 import React, { useState } from 'react';
 import { Document, Page} from 'react-pdf'
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
 
   const [numPages, setNumPages ] = useState(null)
   const [pageNumber, setPageNumber] = useState(1)
+  const files = [
+    {
+      name: "1.pdf",
+      visible: true
+    },
+    {
+      name: "2.pdf",
+      visible: true
+    },
+    {
+      name: "3.pdf",
+      visible: false
+    },
+    {
+      name: "sab15jv.pdf",
+      visible: true
+    },
+    
+  ]
 
   let onDocumentLoadSuccess = ({numPages}) => {
     setNumPages(numPages)
     // setPageNumber(1)
   }
 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Document
-       file={ '/files/2.pdf' }
-       options={{ workerSrc: "/pdf.worker.js" }}
-       onLoadSuccess={onDocumentLoadSuccess}
-       >
-         <Page pageNumber={pageNumber} />
-       </Document>
-       <p>{pageNumber} of {numPages}</p>
+      <div>
+        <ul>
+          {
+            files.map(file  => {
+            if(file.visible){
+              return (
+                <li>{file.name}</li>
+              )}
+            })
+          }
+        </ul>
+
+      </div>
+      {
+        files.map(file  => {
+          if(file.visible){
+            return (
+            <Document
+                file={`files/${file.name}`}
+                options={{ workerSrc: "/pdf.worker.js" }}
+                onLoadSuccess={onDocumentLoadSuccess}
+              >
+               <Page pageNumber={pageNumber} />
+              </Document>
+
+            )}
+          
+        })
+      }
     </div>
   );
 }
