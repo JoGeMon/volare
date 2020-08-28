212417f21b164ef4d58df4c6bbf6fd5ad5500f59
Jorge Velarde <jvelarde@eclass.cl>
2020-08-28 01:12:20 -0400

    feat: mvp1

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import { Document, Page} from 'react-pdf'
 
 function App() {
-
-  const [numPages, setNumPages ] = useState(null)
+  
+  const [numPages, setNumPages] = useState(null)
   const [pageNumber, setPageNumber] = useState(1)
-  const files = [
+  const [files, setFiles] = useState([])
+
+  const ops = [
     {
       name: "1.pdf",
       visible: true
@@ -25,42 +27,49 @@ function App() {
     
   ]
 
+
   let onDocumentLoadSuccess = ({numPages}) => {
     setNumPages(numPages)
     // setPageNumber(1)
+  }
+
+  
+
+  const addToList = (e, name) => {
+    const file = { name, visible: 0 }
+    setFiles(previas => [
+      ...previas,
+      file
+    ])
+
   }
 
 
   return (
     <div className="App">
       <div>
-        <ul>
           {
-            files.map(file  => {
-            if(file.visible){
+            ops.map(op  => {
               return (
-                <li>{file.name}</li>
-              )}
+                  <button key={op.name} onClick={(e) => addToList(e, op.name)}>{op.name}</button>
+              )
             })
           }
-        </ul>
-
       </div>
       {
         files.map(file  => {
-          if(file.visible){
-            return (
+          return (
             <Document
-                file={`files/${file.name}`}
-                options={{ workerSrc: "/pdf.worker.js" }}
-                onLoadSuccess={onDocumentLoadSuccess}
-              >
+              file={`files/${file.name}`}
+              options={{ workerSrc: "/pdf.worker.js" }}
+              onLoadSuccess={onDocumentLoadSuccess}
+              key={file.name}
+            >
                <Page pageNumber={pageNumber} />
-              </Document>
-
-            )}
-          
+            </Document>
+          )
         })
+          
       }
     </div>
   );
