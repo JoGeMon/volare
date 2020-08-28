0e0e1f82689860342ac81e9009887933a58730b9
Jorge Velarde <jvelarde@eclass.cl>
2020-08-28 12:11:18 -0400

    Updates

---
+++ precache-manifest.597bd4245e73f0e7f6de2807e30594e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43a57104081da0d9ed87e28801c37d4d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fc5ecab049f697e90636",
+    "url": "./static/js/2.21331195.chunk.js"
+  },
+  {
+    "revision": "908678959397f2e04ac5f8f7afda467f",
+    "url": "./static/js/2.21331195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79672a8205b1b1f54070",
+    "url": "./static/js/main.ce15838c.chunk.js"
+  },
+  {
+    "revision": "5167ea652cdfc0b896a2",
+    "url": "./static/js/runtime-main.0d571b06.js"
+  }
+]);
