eb8ddd81e1f4dd52747b523f1554be66c2bef19c
Jorge Velarde <jvelarde@eclass.cl>
2020-08-28 12:18:52 -0400

    Updates

---
+++ precache-manifest.7d76745c68c7f175a076a251d8d2b885.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86159716ad0010b1b9bae9922acb77be",
+    "url": "/volare/index.html"
+  },
+  {
+    "revision": "fc5ecab049f697e90636",
+    "url": "/volare/static/js/2.21331195.chunk.js"
+  },
+  {
+    "revision": "908678959397f2e04ac5f8f7afda467f",
+    "url": "/volare/static/js/2.21331195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8f6419325d02003ac22",
+    "url": "/volare/static/js/main.4c55b330.chunk.js"
+  },
+  {
+    "revision": "3b612cb0bef219e907cf",
+    "url": "/volare/static/js/runtime-main.4bf757fb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.597bd4245e73f0e7f6de2807e30594e4.js"
+  "/volare/precache-manifest.7d76745c68c7f175a076a251d8d2b885.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/volare/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
